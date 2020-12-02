edc8f921d2571f33445c333da6214760ea850e2d
TheSlayer666 <55629140+TheSlayer-666@users.noreply.github.com>
2020-12-02 11:36:40 +0200

    Update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ bot.on("message", async message => {
     .split(" ")[0];	
 
     if (command === "gen") {	
-        if(message.channel.id !== "783391433092366356") return message.channel.send("This command can be runned only in the generate channel")	
+        if(message.channel.id !== "783474366365171722") return message.channel.send("This command can be runned only in the generate channel")	
 
         if (generated.has(message.author.id)) {	
             message.channel.send("Wait before generating another account!. - " + message.author);	
